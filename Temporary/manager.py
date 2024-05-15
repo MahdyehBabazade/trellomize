64bbfcf616ac6d8348eace6b774ed50b8fdd7846
MahdyehBabazade <babazademahdye@gmail.com>
2024-05-15 12:52:26 +0330

    purge-data added

--- Temporary/manager.py
+++ Temporary/manager.py
@@ -1,68 +1,35 @@
 import argparse
-#import Temporary.CreateAdmin as CreateAdmin
-import json
-import hashlib
 
-#def create_admin(username, password):
-#    print(f"Username: {username}, Password: {password}")
-#   
-def hashing(password):
-    sha256 = hashlib.sha256()
-    sha256.update(password)
-    return sha256.hexdigest()
+def create_admin(username, password):
+    print(f"Username: {username}, Password: {password}")
 
-def SignUp(email, username, password):
-    def check(email, username):
-        isValid = True
-        for i in range(len(email)):
-            if email[i]=='@':
-                if email[i:]!='gmail.com' or email[i:]!='yahoo.com' or i == 0:
-                    isValid = False
-                    break
+def purge_data():
+    confirmation = input("Are you sure you want to purge all data? This action cannot be undone. (yes/no): ")
+    if confirmation.lower() == 'yes':
+        print("Purging all data...")
+        pass
+        print("All data has been purged.")
+    else:
+        print("Purge data operation canceled.")
 
-        if not isValid:
-            raise Exception('Invalid Email.')
-        elif len(username) == 0:
-            isValid = False
-            raise Exception('Enter username!!')
-        elif len(password) < 8:
-            isValid = False
-            raise Exception('Password must be 8 or more characters.')
-           
-        if isValid:
-            with open('AllFiles.users', 'a') as file: #this is for appending in file we can not read email in file , fix it
-                if email in json.load(file) or username in json.load(file):
-                    raise Exception('This email or username exist. Try another one or use Login.')
-                else:
-                    file.append(json.dumps({"Email":email, "Username":username, "Password":hashing(password)}))
-        
-def log_in(email , username , password):
-    def check(username , password):
-        with open('AllFiles.users', 'r') as file:
-            if username in json.load(file):
-                correct_password = json.load(file)[username]["Password"]
-                this_password = hashing(password)
-                if this_password == correct_password:
-                    pass
-                    #constructor
-                else:
-                    raise Exception('Invalid password!!')
-            else:
-                raise Exception('Invalid username!!')
-                
 def execute_command():
-    parser = argparse.ArgumentParser(description='Create an Admin')
+    parser = argparse.ArgumentParser(description='Admin Management')
     subparser = parser.add_subparsers(dest='command', help='sub-command help')
     create_admin_parser = subparser.add_parser('create-admin', help='Create admin user')
-    create_admin_parser.add_argument('--username', type=str, required=True,  help='The username of the admin')
+    create_admin_parser.add_argument('--username', type=str, required=True, help='The username of the admin')
     create_admin_parser.add_argument('--password', type=str, required=True, help='The password of the admin')
+    purge_data_parser = subparser.add_parser('purge-data', help='Purge all data')
     args = parser.parse_args()
+    
     if args.command == 'create-admin':
         if args.username and args.password:
-            SignUp(args.username, args.password)
+            create_admin(args.username, args.password)
         else:
             print('You should provide both username and password.')
+    elif args.command == 'purge-data':
+        purge_data()
     else:
-        print("Invalid command. Use 'create-admin'.")
+        print("Invalid command. Use 'create-admin' or 'purge-data'.")
+
 if __name__ == "__main__":
     execute_command()
